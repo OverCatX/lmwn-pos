c7beafec23500685db4486eae4cd847e8cdd2e25
Bhumipat Kusalatham <overcatpocket@gmail.com>
2026-02-03 19:21:37 +0700

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { HealthService } from './common/health/health.service';
+import { getDatabaseConfig } from './config/database.config';
+import appConfig from './config/app.config';
+
+@Module({
+  imports: [
+    // Configuration Module
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [appConfig],
+      envFilePath: ['.env.local', '.env'],
+    }),
+    // TypeORM Module
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: getDatabaseConfig,
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService, HealthService],
+})
+export class AppModule { }
---
+++ src/config/app.config.ts
@@ -0,0 +1,9 @@
+import { registerAs } from '@nestjs/config';
+
+export default registerAs('app', () => {
+    return {
+        port: parseInt(process.env.PORT || '3000', 10),
+        env: process.env.NODE_ENV || 'development',
+        apiPrefix: process.env.API_PREFIX || 'api',
+    };
+});
